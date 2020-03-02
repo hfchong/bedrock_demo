f85d14bc097a05fe48f9042a55af783de0ecfd0d
Chong Hon Fah <honfah@basis-ai.com>
2020-03-02 12:10:59 +0800

    Update train.py

--- train.py
+++ train.py
@@ -9,69 +9,57 @@ import shutil
 logger = logging.getLogger(__name__)
 bedrock = BedrockApi(logger)
 
+
 def log_learnloss(output):
     bedrock.log_metric("loss", float(re.search('(?<=, ).*(?= avg)', output).group().strip()))
     bedrock.log_metric("loss_iteration", int(re.search("\d*", output.split()[0]).group().strip()))
     bedrock.log_metric("learn", float(re.search('(?<=loss, ).*(?= rate)', output).group().strip()))
     bedrock.log_metric("learn_iteration", int(re.search("\d*", output.split()[0]).group().strip()))
 
-def run_command(command):
-    print(command)
+
+def run_command(command, logfile):
+    logfile.write("This is the run command: %s\n" % command)
     process = subprocess.Popen(shlex.split(command), stdout=subprocess.PIPE, encoding='utf8')
-    temp_map_val = ""
-    map_ctr = 0
+    currentMAP = 0.0
+    currentIterationNumber = 0
     while True:
         output = process.stdout.readline()
+        logFile.write(output + "\n")
         if output == '' and process.poll() is not None:
+            break
+
+        elif (currentMAP >= float(os.environ['MAP_STOP'])) or (currentIterationNumber >= int(os.environ['ITERATIONS_STOP'])):
+            logFile.write("Current iteration number: %d\n" % (currentIterationNumber))
+            logFile.write("Current mAP :%f\n" % (currentMAP))
             break
 
         elif output == '':
             continue
 
         elif "mean average precision (mAP@0.50)" in output:
-            map_ctr = 1
-            temp_map_val = output
-
-        elif map_ctr >= 1:
-            if 'avg loss' in output:
-                bedrock.log_metric("mAP", float(temp_map_val.split()[7]));
-                bedrock.log_metric("mAP_iteration_number", int(output.split(":")[0].strip()) - 1)
-                log_learnloss(output)
-                map_ctr = 0
-                temp_map_val = ""
-
-            elif map_ctr > 0 and map_ctr < 400:
-                map_ctr += 1
-
-            else:
-                map_ctr = 0
-                temp_map_val = ""
+            bedrock.log_metric("mAP", float(output.split()[7]));
+            currentMAP = float(output.split()[7])
 
         elif re.search('(?<=, ).*(?= avg)', output):
+            currentIterationNumber = int(re.search("\d*", output.split()[0]).group().strip())
             log_learnloss(output)
 
-        print(map_ctr)
-        print(output)
+    print("Training Ended")
 
     rc = process.poll()
     return rc
 
 
-weight = os.getenv('weights', 'darknet.conv.74')
-gpus = os.getenv('gpus', '0')
-cfg = os.getenv('cfg', 'yolov3/cfg')
-data = os.getenv('data', 'yolo')
-path = "./"
-destination = '/root/darknet'
+logFile = open(os.environ['LOGFILE'], "a+")
 
-url = "http://pjreddie.com/media/files/darknet53.conv.74"
+os.chdir("/root/darknet")
+logFile.write("Current working directory: %s\n" % (os.getcwd()))
+logFile.write("Weights :%s\n" % (os.environ['WEIGHTS']))
+logFile.write("CFG :%s\n" % (os.environ['CFG']))
+logFile.write("DATA :%s\n" % (os.environ['DATA']))
+logFile.write("Stop at iteration number: %s\n" % (os.environ['ITERATIONS_STOP']))
+logFile.write("Stop at mAP :%s\n" % (os.environ['MAP_STOP']))
 
-# subprocess.run(["wget", "-O", path + url.split("/")[-1], url])
-# dest = shutil.move(path, destination)
+run_command("./darknet detector train data/%s %s %s -map" % (os.environ['DATA'], os.environ['CFG'], os.environ['WEIGHTS']), logFile)
 
-
-os.chdir("/home/nazar/NeuralNetworkFramework/Darknet")
-#os.chdir("/root/darknet")
-print(os.getcwd())
-
-# run_command("./darknet detector train data/rotifer.data rotifer_exp01.cfg rotifer_exp01_best.weights -gpus %s -map" %(gpus))
+logFile.close()
